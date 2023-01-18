a0ceda81e9ad9463931c745b2ba50a426bcfffac
viktor1798 <vktrsmsk1798@gmail.com>
2023-01-18 08:07:29 +0300

    add adaptive

--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
-let i=1, count=3;
+// let i=1, count=3;
 
-function imgSlide(){
-    if(i > count){
-        i =1
-    }
-    slider.src =`images/autor${i++}.png`
-    setTimeout(imgSlide,3000);
-}
+// function imgSlide(){
+//     if(i > count){
+//         i =1
+//     }
+//     slider.src =`images/autor${i++}.png`
+//     setTimeout(imgSlide,3000);
+// }
